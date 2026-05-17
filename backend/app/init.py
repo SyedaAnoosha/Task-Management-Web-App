c694fbea88cda974ba0c0e67915fff83f454d53d
Anoosha <anushazaidi06@gmail.com>
2026-05-17 12:35:32 +0500

    tested authentication system

--- backend/app/init.py
+++ backend/app/init.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 from app.config import Config
 from app.database import db
-from app.routes import bp as task_routes
+from app.routes.auth_routes import auth_routes
+from app.routes.task_routes import task_routes
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_object(Config)
 
     CORS(app)
 
     db.init_app(app)
 
+    JWTManager(app)
+
     app.register_blueprint(task_routes)
+    app.register_blueprint(auth_routes)
 
     with app.app_context():
         db.create_all()
