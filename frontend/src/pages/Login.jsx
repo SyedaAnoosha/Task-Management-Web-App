d7d3d674c711d11eb101434bd3114e7f0635967a
Anoosha <anushazaidi06@gmail.com>
2026-05-17 12:49:44 +0500

    added login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { loginUser } from "../services/api";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await loginUser({ username, password });
+
+      localStorage.setItem("token", res.data.access_token);
+
+      navigate("/");
+    } catch (err) {
+      alert(err.response?.data?.error || "Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <p>
+        No account? <Link to="/register">Register here</Link>
+      </p>
+    </div>
+  );
+}
